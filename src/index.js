e8a0e34d6d26e887c472ab8a95280a742cb389c4
zaidi mounib <mounibzaidi@gmail.com>
2023-09-28 11:45:43 +0100

    Assignment Completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-const init = () => {
-  
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const input = document.querySelector("input#searchByID");
+
+        const userInput = input.value;
+
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        fetch(`http://localhost:3000/movies/${userInput}`)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Movie not found");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                title.innerText = data.title;
+                summary.innerText = data.summary;
+            })
+            .catch((error) => {
+                title.innerText = "Movie not found";
+                summary.innerText = "";
+            });
+    });
+});
